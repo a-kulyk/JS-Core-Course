5038cefa557db84f405f85541c23001e96bb1fd0
Andrii Kulyk <akuly@softserveinc.com>
2015-12-25 14:07:49 +0200

    Multi Instance working

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-(function initToDoList() {
+(function () {
+    var newList = document.getElementById('newList');
+    newList.addEventListener("click", onNewList);
 
-    var textBox = document.getElementById('textBox');
-    var list = document.getElementById('todoList');
-    var addButton = document.getElementById('btnAdd');
-    textBox.addEventListener("keypress", function() {
-        if (event.keyCode == 13) onAddItem();
-    });
-    addButton.addEventListener("click", onAddItem);
 
-    function onAddItem() {
-        var entry = document.createElement('tr');
-        var cell = document.createElement('td');
-        entry.appendChild(cell).innerHTML = textBox.value;
-        if (textBox.value) {
-            entry.appendChild(createDeleteButton());
-            list.appendChild(entry);
-            textBox.value = "";
+
+
+    function onNewList() {
+        var instance = document.createElement('div');
+        var textBox = document.createElement('input');
+        textBox.className = "textBox";
+        textBox.setAttribute("placeholder", "Add your Todo item");
+        var addButton = document.createElement('button');
+        addButton.innerHTML = "Add Item";
+        var closeButton = document.createElement('input');
+        closeButton.setAttribute("type", "image");
+        closeButton.setAttribute("src", "close.png");
+        var list = document.createElement('table');
+
+
+        document.body.appendChild(instance);
+        instance.appendChild(textBox);
+        instance.appendChild(addButton);
+        instance.appendChild(closeButton);
+        instance.appendChild(list);
+        textBox.addEventListener("keypress", function() {
+                if (event.keyCode == 13) onAddItem();
+        });
+        addButton.addEventListener("click", onAddItem);
+        closeButton.addEventListener("click", onRemoveItem);
+
+        function onAddItem() {
+            var entry = document.createElement('tr');
+            var cell = document.createElement('td');
+            entry.appendChild(cell).innerHTML = textBox.value;
+            if (textBox.value) {
+                entry.appendChild(createDeleteButton());
+                list.appendChild(entry);
+                textBox.value = "";
+            }
+            textBox.focus();
         }
-    }
 
-    function createDeleteButton() {
-        var deleteButton = document.createElement('button');
-        deleteButton.setAttribute("class", "btnDelete");
-        deleteButton.innerHTML = "Remove item";
-        deleteButton.addEventListener("click", onRemoveItem);
-        return deleteButton;
-    }
+        function createDeleteButton() {
+            var deleteButton = document.createElement('button');
+            deleteButton.setAttribute("class", "btnDelete");
+            deleteButton.innerHTML = "Remove item";
+            deleteButton.addEventListener("click", onRemoveItem);
+            return deleteButton;
+        }
 
-    function onRemoveItem() {
-        var parent = this.parentNode.parentNode;
-        var child = this.parentNode;
+        function onRemoveItem() {
+            var parent = this.parentNode.parentNode;
+            var child = this.parentNode;
 
-        parent.removeChild(child);
+            parent.removeChild(child);
+        }
     }
 })();
