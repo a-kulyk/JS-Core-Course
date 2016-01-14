e6737fe30bf8a64837c597b9c478b0d62849af2f
Andrii Kulyk <akuly@softserveinc.com>
2016-01-14 16:50:50 +0200

    improved module

--- script.js
+++ script.js
@@ -3,38 +3,36 @@ var myModule = (function() {
     function init(rootId) {
         var div = document.getElementById(rootId);
         var textBox = div.getElementsByTagName('input')[0];
-        var addButton = div.getElementsByTagName('button')[0];
         var list = document.createElement('table');
         div.appendChild(list);
         textBox.addEventListener("keypress", function() {
-            if (event.keyCode == 13) onAddItem();
+            if (event.keyCode == 13) onAddItem(list, textBox);
         });
+    }
 
-        function onAddItem() {
-            var entry = document.createElement('tr');
-            var cell = document.createElement('td');
+    function onAddItem(list, textBox) {
+        var entry = document.createElement('tr');
+        var cell = document.createElement('td');        
+        if (textBox.value) {
             entry.appendChild(cell).innerHTML = "- " + textBox.value;
-            if (textBox.value) {
-                entry.appendChild(createDeleteButton());
-                list.appendChild(entry);
-                textBox.value = null;
-            }
-            textBox.focus();
+            entry.appendChild(createDeleteButton());
+            list.appendChild(entry);
+            textBox.value = null;
         }
+    }
 
-        function createDeleteButton() {
-            var deleteButton = document.createElement('button');
-            deleteButton.setAttribute("class", "btnDelete");
-            deleteButton.innerHTML = "Remove";
-            deleteButton.addEventListener("click", onRemoveItem);
-            return deleteButton;
-        }
+    function createDeleteButton() {
+        var deleteButton = document.createElement('button');
+        deleteButton.setAttribute("class", "btnDelete");
+        deleteButton.innerHTML = "Remove";
+        deleteButton.addEventListener("click", onRemoveItem);
+        return deleteButton;
+    }
 
-        function onRemoveItem() {
-            var child = this.parentNode;
-            var parent = child.parentNode;
-            parent.removeChild(child);
-        }
+    function onRemoveItem() {
+        var child = this.parentNode;
+        var parent = child.parentNode;
+        parent.removeChild(child);
     }
 
     return {
