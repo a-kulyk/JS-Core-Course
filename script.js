56cff2815f1755979ef2e4e64e8c8fc6eb16fb96
Andrii Kulyk <akuly@softserveinc.com>
2016-01-14 13:57:22 +0200

    modular structure added

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-(function () {
-    var newList = document.getElementById('newList');
-    newList.addEventListener("click", onNewList);
+var myModule = (function() {
 
-
-
-
-    function onNewList() {
-        var instance = document.createElement('div');
-        var textBox = document.createElement('input');
-        textBox.className = "textBox";
-        textBox.setAttribute("placeholder", "Add your Todo item");
-        var addButton = document.createElement('button');
-        addButton.innerHTML = "Add Item";
-        var closeButton = document.createElement('input');
-        closeButton.setAttribute("type", "image");
-        closeButton.setAttribute("src", "close.png");
+    function init(rootId) {
+        var div = document.getElementById(rootId);
+        var textBox = div.getElementsByTagName('input')[0];
+        var addButton = div.getElementsByTagName('button')[0];
         var list = document.createElement('table');
-
-
-        document.body.appendChild(instance);
-        instance.appendChild(textBox);
-        instance.appendChild(addButton);
-        instance.appendChild(closeButton);
-        instance.appendChild(list);
+        div.appendChild(list);
         textBox.addEventListener("keypress", function() {
-                if (event.keyCode == 13) onAddItem();
+            if (event.keyCode == 13) onAddItem();
         });
-        addButton.addEventListener("click", onAddItem);
-        closeButton.addEventListener("click", onRemoveItem);
 
         function onAddItem() {
             var entry = document.createElement('tr');
             var cell = document.createElement('td');
-            entry.appendChild(cell).innerHTML = textBox.value;
+            entry.appendChild(cell).innerHTML = "- " + textBox.value;
             if (textBox.value) {
                 entry.appendChild(createDeleteButton());
                 list.appendChild(entry);
-                textBox.value = "";
+                textBox.value = null;
             }
             textBox.focus();
         }
@@ -44,16 +25,22 @@
         function createDeleteButton() {
             var deleteButton = document.createElement('button');
             deleteButton.setAttribute("class", "btnDelete");
-            deleteButton.innerHTML = "Remove item";
+            deleteButton.innerHTML = "Remove";
             deleteButton.addEventListener("click", onRemoveItem);
             return deleteButton;
         }
 
         function onRemoveItem() {
-            var parent = this.parentNode.parentNode;
             var child = this.parentNode;
-
+            var parent = child.parentNode;
             parent.removeChild(child);
         }
     }
+
+    return {
+        create: init
+    }
 })();
+
+myModule.create('firstList');
+myModule.create('secondList');
