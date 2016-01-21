3a01e15eb9842443ccafd9369fcc73fa45ef9ec7
Andrii Kulyk <akuly@softserveinc.com>
2016-01-21 12:32:25 +0200

    some corrections

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var myModule = (function() {
 
+    var DEL_BTN_CLS = 'btnDelete';
+    var DEL_BTN_INN_HTML = "Remove";
+
     function init(rootId) {
         var div = document.getElementById(rootId);
         var textBox = div.getElementsByTagName('input')[0];
+        var addButton = div.getElementsByTagName('button')[0];
         var list = document.createElement('table');
         div.appendChild(list);
         textBox.addEventListener("keypress", function() {
             if (event.keyCode == 13) onAddItem(list, textBox);
+        });
+        addButton.addEventListener("click", function() {
+            onAddItem(list, textBox)
         });
     }
 
@@ -23,8 +30,8 @@ var myModule = (function() {
 
     function createDeleteButton() {
         var deleteButton = document.createElement('button');
-        deleteButton.setAttribute("class", "btnDelete");
-        deleteButton.innerHTML = "Remove";
+        deleteButton.setAttribute("class", DEL_BTN_CLS);
+        deleteButton.innerHTML = DEL_BTN_INN_HTML;
         deleteButton.addEventListener("click", onRemoveItem);
         return deleteButton;
     }
